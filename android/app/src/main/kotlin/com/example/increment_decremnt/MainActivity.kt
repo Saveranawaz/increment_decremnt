71b3dfa914a7a4b485e5bfcae31f1335ce0aa3a4
Saveranawaz <saveranawaz77@gmail.com>
2024-11-21 21:26:09 -0800

    ubdate

---
+++ android/app/src/main/kotlin/com/example/increment_decremnt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.increment_decremnt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/incremnt_decremnt.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+class _CounterState extends State<Counter> {
+  int _counter = 0;  // Initial count value
+  void _incrementCounter() {
+    setState(() {
+      _counter++;  // Increment count
+    });
+  }
+  void _decrementCounter(){
+    setState(() {
+      _counter--;  // Decrement count
+    });
+  }
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;  // Reset count to 0
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Counter',style: TextStyle(color:Colors.black,fontSize: 50),),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Count: $_counter',
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // Decrement Button (-)
+                  ElevatedButton(
+                    onPressed: _decrementCounter,
+                    child: Icon(Icons.remove),
+                  ),
+                  // Increment Button (+)
+                  ElevatedButton(
+                    onPressed: _incrementCounter,
+                    child: Icon(Icons.add),
+                  ),
+                  // Reset Button (Refresh)
+                  ElevatedButton(
+                    onPressed: _resetCounter,
+                    child: Icon(Icons.refresh),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
